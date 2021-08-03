80d7a766bc257ae0a1fa2d8f734a7e1c19e7f8d7
Diego Raul <devup2332@gmail.com>
2021-08-03 12:17:03 -0500

    Changed build angular limits of size of each file

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  emailPatt:
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  numberPatt: /^[0-9]+$/,
+  backend_uri: `http://localhost:8000`,
 };
